9c736deff51e0e919010166683076abf0d8eaf54
Erwannnito <erwann.barre56@gmail.com>
2025-03-05 16:39:01 +0100

    page result faite

--- script-result.js
+++ script-result.js
@@ -1,23 +1,74 @@
 document.addEventListener("DOMContentLoaded", function () {
+    
+    // Récupération des scores et initialisation des catégories et phrases de description
     const X = JSON.parse(localStorage.getItem("scores"));
-    const categories = ["Consommation Energétique", "Dépendance", "Morale", "Pertinence de l'utilisation"];        
+    const categories = ["Consommation Energétique", "Dépendance", "Morale", "Pertinence de l'utilisation"];  
+    const phrases = [
+        "Tu connais l'IA ? (Boomer)",
+        "Tu es fraîchement défloré de l'IA",
+        "Tu trouves l'IA vachement pratique quand même",
+        "T'es un étudiant scientifique lambda",
+        "Tu ne penses plus par toi-même",
+        "Tu es en couple avec chat GPT",
+        "Tu es un bandeur d'IA"
+    ] 
+    const ConsoEnergy = X[0];  // copie de X[0] qui ne sera pas modifiée
+    X[0] = 100.3637 * (1 - Math.exp(-0.2 * X[0]));  // transformation de la consommation énergétique en score sur 100
+    const totalScore = X.reduce((a, b) => a + b, 0).toFixed(0); // calcul du score total
 
-    document.getElementById("totalScore").innerText = X.reduce((a, b) => a + b, 0);
-            
+    // Affichage de la phrase de description
+    const descriptElement = document.getElementById("descript");
+    let phraseIndex;
+    if (totalScore < 100) {
+        phraseIndex = 0;
+    } else if (totalScore < 150) {
+        phraseIndex = 1;
+    } else if (totalScore < 200) {
+        phraseIndex = 2;
+    } else if (totalScore < 250) {
+        phraseIndex = 3;
+    } else if (totalScore < 300) {
+        phraseIndex = 4;
+    } else if (totalScore < 350) {
+        phraseIndex = 5;
+    } else {
+        phraseIndex = 6;
+    }
+    descriptElement.innerText = phrases[phraseIndex];
+
+
+    // Affichage du score total
+    const totalScoreElement = document.getElementById("totalScore");
+    totalScoreElement.innerText = totalScore;
+
+    if (totalScore < 100) {
+        totalScoreElement.style.backgroundColor = "rgb(117, 201, 117)";
+    } else if (totalScore < 200) {
+        totalScoreElement.style.backgroundColor = "rgb(116, 189, 248)";
+    } else if(totalScore < 300) {
+        totalScoreElement.style.backgroundColor = "rgb(235, 175, 91)";
+    } else {
+        totalScoreElement.style.backgroundColor = "rgb(216, 102, 98)";
+    }
+     
+    // Affichage des scores des différentes catégories
     const boxesContainer = document.getElementById("boxes");
     X.forEach((score, index) => {
+        
         let box = document.createElement("div");
         box.className = "box";
-        if (score < 30) {
+
+        if (score < 25) {
             box.style.backgroundColor = "rgb(117, 201, 117)";
-        } else if (score < 55) {
+        } else if (score < 50) {
             box.style.backgroundColor = "rgb(116, 189, 248)";
         } else if (score < 80) {
             box.style.backgroundColor = "rgb(235, 175, 91)";
         } else {
             box.style.backgroundColor = "rgb(216, 102, 98)";
         }
-        box.innerText = `${categories[index]}: ${score}%`;
+        
+        box.innerText = `${categories[index]} : ${score.toFixed(0)}%`;
         boxesContainer.appendChild(box);
     });
 });
